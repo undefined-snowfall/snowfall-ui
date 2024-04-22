734f698786b11c14808fd4999ed954cce6045fe0
Yui Tanabe <walker27460@users.noreply.github.com>
2024-04-22 18:58:11 +0900

    Revert "[DO NOT MERGE (WIP)] feat(web): first alpha implementation (#9)"
    This reverts commit f535afa4c758a2f0dac0516b0db68495d55e5190.

--- packages/web/scripts/build.js
+++
@@ -1,9 +0,0 @@
-const result = await Bun.build({
-  minify: Bun.env.NODE_ENV === 'production',
-  entrypoints: ['lib/index.js'],
-  outdir: 'dist'
-})
-
-for (const log of result.logs) {
-  console.log(log)
-}
